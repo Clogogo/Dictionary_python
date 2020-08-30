82d1b9744f6ae34eab5b45db849d7dfc0407201f
Lucky <chigoziecigo@yahoo.com>
2020-08-30 19:48:48 +0300

    created function

--- frontend.py
+++ frontend.py
@@ -14,49 +14,56 @@ User can:
 """
 
 from tkinter import *
-import backend
+from backend import Database
+
+database = Database("books.db")
 
 
-def get_selected_row(event):
-    global selected_tuple
-    index = list1.curselection()[0]
-    selected_tuple = list1.get(index)
-    e1.delete(0, END)
-    e1.insert(END, selected_tuple[1])
-    e2.delete(0, END)
-    e2.insert(END, selected_tuple[2])
-    e3.delete(0, END)
-    e3.insert(END, selected_tuple[3])
-    e4.delete(0, END)
-    e4.insert(END, selected_tuple[4])
+class Frontend:
+
+    def __init__(self):
+        pass
+
+    def get_selected_row(self):
+        try:
+            global selected_tuple
+            index = list1.curselection()[0]
+            selected_tuple = list1.get(index)
+            e1.delete(0, END)
+            e1.insert(END, selected_tuple[1])
+            e2.delete(0, END)
+            e2.insert(END, selected_tuple[2])
+            e3.delete(0, END)
+            e3.insert(END, selected_tuple[3])
+            e4.delete(0, END)
+            e4.insert(END, selected_tuple[4])
+
+        except IndexError:
+            pass
+
+    def view_command(self):
+        list1.delete(0, END)
+        for row in database.view():
+            list1.insert(END, row)
+
+    def search_command(self):
+        list1.delete(0, END)
+        for row in database.search(title_text.get(), author_text.get(), year_text.get(), isbn_text.get()):
+            list1.insert(END, row)
+
+    def add_command(self):
+        database.insert(title_text.get(), author_text.get(), year_text.get(), isbn_text.get())
+        list1.delete(0, END)
+        list1.insert(END, (title_text.get(), author_text.get(), year_text.get(), isbn_text.get()))
+
+    def delete_command(self):
+        database.delete(selected_tuple[0])
+
+    def update_command(self):
+        database.update(selected_tuple[0], title_text.get(), author_text.get(), year_text.get(), isbn_text.get())
 
 
-def view_command():
-    list1.delete(0, END)
-    for row in backend.view():
-        list1.insert(END, row)
-
-
-def search_command():
-    list1.delete(0, END)
-    for row in backend.search(title_text.get(), author_text.get(), year_text.get(), isbn_text.get()):
-        list1.insert(END, row)
-
-
-def add_command():
-    backend.insert(title_text.get(), author_text.get(), year_text.get(), isbn_text.get())
-    list1.delete(0, END)
-    list1.insert(END, (title_text.get(), author_text.get(), year_text.get(), isbn_text.get()))
-
-
-def delete_command():
-    backend.delete(selected_tuple[0])
-
-
-def update_command():
-    backend.update(selected_tuple[0], title_text.get(), author_text.get(), year_text.get(), isbn_text.get())
-
-
+front = Frontend()
 window = Tk()
 
 window.wm_title("BookStore")
@@ -98,21 +105,21 @@ sb1.grid(row=2, column=2, rowspan=6)
 list1.configure(yscrollcommand=sb1.set)
 sb1.configure(command=list1.yview)
 
-list1.bind('<<ListboxSelect>>', get_selected_row)
+list1.bind('<<ListboxSelect>>', front.get_selected_row)
 
-b1 = Button(window, text="View all", width=12, command=view_command)
+b1 = Button(window, text="View all", width=12, command=front.view_command)
 b1.grid(row=2, column=3)
 
-b2 = Button(window, text="Search entry", width=12, command=search_command)
+b2 = Button(window, text="Search entry", width=12, command=front.search_command)
 b2.grid(row=3, column=3)
 
-b3 = Button(window, text="Add entry", width=12, command=add_command)
+b3 = Button(window, text="Add entry", width=12, command=front.add_command)
 b3.grid(row=4, column=3)
 
-b4 = Button(window, text="Update selected", width=12, command=update_command)
+b4 = Button(window, text="Update selected", width=12, command=front.update_command)
 b4.grid(row=5, column=3)
 
-b5 = Button(window, text="Delete selected", width=12, command=delete_command)
+b5 = Button(window, text="Delete selected", width=12, command=front.delete_command)
 b5.grid(row=6, column=3)
 
 b6 = Button(window, text="Close", width=12, command=window.destroy)
